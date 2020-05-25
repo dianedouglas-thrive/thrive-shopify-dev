3469384bd747e35345f40b94e95f0f907fa0ae8e
Diane Douglas <dianedouglas@Dianes-MacBook-Air.local>
2020-05-24 23:52:58 -0400

    Add static js file and update initializer to ask for access to script tags scope and add test script.

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,12 +3,15 @@ ShopifyApp.configure do |config|
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
   config.old_secret = ""
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "read_products, write_script_tags" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
   config.api_version = "2020-04"
   config.shop_session_repository = 'Shop'
+  config.scripttags = [
+    {event:'onload', src: 'https://thrive-commerce-test.myshopify.com/assets/test.js'}
+  ]
 end
 
 # ShopifyApp::Utils.fetch_known_api_versions                        # Uncomment to fetch known api versions from shopify servers on boot
