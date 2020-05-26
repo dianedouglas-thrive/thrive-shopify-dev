e42fa910dcba86c20e13b6e00772c01a40285f86
Diane Douglas <dianedouglas@Dianes-MacBook-Air.local>
2020-05-26 14:30:03 -0400

    Render js erb file instead of using assets path.

---
+++ app/controllers/script_controller.rb
@@ -0,0 +1,3 @@
+class ScriptController < ActionController::Base
+  protect_from_forgery except: :test
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'script/test', to: 'script#test', :defaults => { format: :js }
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
