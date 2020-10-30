32db2c8ed9939a89f56ace30ab0d9e303294438a
Diane Douglas <dianedouglas@Dianes-MacBook-Air.local>
2020-10-30 14:33:52 -0400

    Add working query for order by id as variable from webhook getting back email as test data.

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -7,22 +7,37 @@ class OrdersCreateJob < ActiveJob::Base
       return
     end
 
+    # authenticate with current shop session using this block on shop model.
+    # could be done in a custom controller if you want
+    # https://github.com/Shopify/shopify_app#webhooksmanager
     shop.with_shopify_session do
-    	order_id = webhook['id']
 
-    	client = ShopifyAPI::GraphQL.client
+      # get order id out of webhook parameter
+      order_id = webhook['id']
 
-    	shop_name_query = client.parse <<-'GRAPHQL'
-    	  {
-    	    shop {
-    	      name
-    	    }
-    	  }
-    	GRAPHQL
+      # format it for graphql order id argument
+      # takes a string
+      graphql_order_id = "gid://shopify/Order/#{order_id}"
 
-    	result = client.query(shop_name_query)
-    	byebug
+      # grab the client. initialized inside engine.
+      client = ShopifyAPI::GraphQL.client
 
+      # create query structure. 
+      # says expect a variable called $id of type ID! 
+      # type ID! is a scalar from the schema but a string works.
+      # pass $id into the order query as order(id: $id)
+      # request data back from order inside brackets: { email }
+      order_query = client.parse <<-'GRAPHQL'
+        query($id: ID!) {
+          order(id: $id) {
+            email
+          }
+        }
+      GRAPHQL
+
+      # get result from client.query() passing in all variables as a hash in key 'variables'
+      result = client.query(order_query, variables: { id: graphql_order_id })
+      puts result.data.order.email
     end
   end
 end
