ee4b01459ab6a7b0c7f29d7549ab490a59d06530
Diane Douglas <dianedouglas@Dianes-MacBook-Air.local>
2020-11-30 12:53:35 -0500

    Update gql query logic to get customer journey with rerunning query on nil values.

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -7,50 +7,57 @@ class OrdersCreateJob < ActiveJob::Base
       return
     end
 
-    # authenticate with current shop session using this block on shop model.
-    # could be done in a custom controller if you want
-    # https://github.com/Shopify/shopify_app#webhooksmanager
     shop.with_shopify_session do
-      # get graphql order id from webhook
+      # get order id out of webhook parameter
       graphql_order_id = webhook['admin_graphql_api_id']
 
       # grab the client. initialized inside engine.
       client = ShopifyAPI::GraphQL.client
 
       # create query structure. 
-      # says expect a variable called $id of type ID! 
-      # type ID! is a scalar from the schema but a string works.
-      # pass $id into the order query as order(id: $id)
-      # request data back from order inside brackets: { email }
       order_query = client.parse <<-'GRAPHQL'
         query($id: ID!) {
           order(id: $id) {
-            customer {
-              id
-              displayName
-              phone
-              tags
-              acceptsMarketing
-              totalSpent
-              lifetimeDuration
-              locale
-              ordersCount
-            }
+            email
             customerJourneySummary {
-              customerOrderIndex
-              momentsCount
               daysToConversion
               firstVisit {
                 id
                 landingPage
+                landingPageHtml
+                occurredAt
+                referralCode
+                referralInfoHtml
                 referrerUrl
                 source
+                sourceDescription
+                sourceType
+                utmParameters {
+                  campaign
+                  content
+                  medium
+                  source
+                  term
+                }
               }
               lastVisit {
                 id
                 landingPage
+                landingPageHtml
+                occurredAt
+                referralCode
+                referralInfoHtml
                 referrerUrl
                 source
+                sourceDescription
+                sourceType
+                utmParameters {
+                  campaign
+                  content
+                  medium
+                  source
+                  term
+                }
               }
             }
           }
@@ -59,78 +66,13 @@ class OrdersCreateJob < ActiveJob::Base
 
       # get result from client.query() passing in all variables as a hash in key 'variables'
       result = client.query(order_query, variables: { id: graphql_order_id })
-      
-      # get nested data out of results
-      customer_journey = result.data.order.customer_journey_summary
-      customer = result.data.order.customer
-      moments_count = customer_journey.moments_count.to_i
-      orders_count = customer.orders_count.to_i
-      first_visit = customer_journey.first_visit
-      last_visit = customer_journey.last_visit
-      customer_exists = orders_count > 0
-      # now that we have count of moments/orders we know if they are an existing customer or not.
-      if customer_exists
-        puts "---- existing customer. get order history and journey. ----"
-        # pre-existing customer with order history
-        order_query = client.parse <<-'GRAPHQL'
-          query($id: ID!, $total_orders: Int, $total_moments: Int) {
-            order(id: $id) {
-              customer {
-                orders(first: $total_orders){
-                  edges {
-                    node {
-                      id
-                      totalPriceSet{
-                        presentmentMoney{
-                          amount
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-              customerJourneySummary {
-                moments(first: $total_moments) {
-                  edges {
-                    node {
-                      occurredAt
-                    }
-                  }
-                }
-              }
-            }
-          }
-        GRAPHQL
-
-        nested_result = client.query(order_query, variables: { id: graphql_order_id, total_orders: orders_count, total_moments: moments_count })
-      else  
-        puts "---- new customer. get journey only. ----"
-
-        order_query = client.parse <<-'GRAPHQL'
-          query($id: ID!, $total_moments: Int) {
-            order(id: $id) {
-              customerJourneySummary {
-                moments(first: $total_moments) {
-                  edges {
-                    node {
-                      occurredAt
-                    }
-                  }
-                }
-              }
-            }
-          }
-        GRAPHQL
-
-        nested_result = client.query(order_query, variables: { id: graphql_order_id, total_moments: moments_count })
-      end
-
-      if customer_exists
-        # ex: get total price of first order, or any other attribute. could use a loop/map.
-        puts nested_result.data.order.customer.orders.edges.first.node.total_price_set.presentment_money.amount
-      else
-        # ex: access moments array like this.
-        puts nested_result.data.order.customer_journey_summary.moments.edges.count
+      # customer journey summary is not available immediately.
+      total_wait_time = 0
+      while result.data.order.customer_journey_summary.days_to_conversion.nil? && total_wait_time <= 60
+        sleep(15.seconds)
+        result = client.query(order_query, variables: { id: graphql_order_id })
+        total_wait_time += 10
+        puts "Make query."
       end
     end
   end
