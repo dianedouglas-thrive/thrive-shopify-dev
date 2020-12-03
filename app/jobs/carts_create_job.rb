cae122abdb5db2c53c807dbca3bc930f8a4110c1
Diane Douglas <dianedouglas@Dianes-MacBook-Air.local>
2020-12-03 14:22:39 -0500

    Probably don't need these but adding placeholders for cart webhook jobs and other graphql schema versions.

---
+++ app/jobs/carts_create_job.rb
@@ -0,0 +1,14 @@
+class CartsCreateJob < ActiveJob::Base
+  def perform(shop_domain:, webhook:)
+    shop = Shop.find_by(shopify_domain: shop_domain)
+
+    if shop.nil?
+      logger.error("#{self.class} failed: cannot find shop with domain '#{shop_domain}'")
+      return
+    end
+
+    shop.with_shopify_session do
+      puts "******** CART CREATE **********"
+    end
+  end
+end
---
+++ app/jobs/carts_update_job.rb
@@ -0,0 +1,14 @@
+class CartsUpdateJob < ActiveJob::Base
+  def perform(shop_domain:, webhook:)
+    shop = Shop.find_by(shopify_domain: shop_domain)
+
+    if shop.nil?
+      logger.error("#{self.class} failed: cannot find shop with domain '#{shop_domain}'")
+      return
+    end
+
+    shop.with_shopify_session do
+      puts "******** CART UPDATE **********"
+    end
+  end
+end
